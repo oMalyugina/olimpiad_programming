50d454893d00d1eb38c2617d314da782bb8e23d6
oMalyugina <o.v.malyugina@gmail.com>
2019-06-21 21:21:13 +0200

    432

--- graphs/432.cpp
+++ graphs/432.cpp
@@ -7,53 +7,37 @@
 
 using std::vector;
 
-struct Point {
-    int x;
-    int y;
 
-    Point(int x_, int y_) : x(x_), y(y_) {};
-};
-
+void fill(vector<vector<int>> &field, int x, int y){
+    if (x < 0 or y < 0 or x >= field.size() or y >= field[0].size()) return;
+    if (field[x][y] != 0) return;
+    field[x][y]=1;
+    fill(field, x-1, y);
+    fill(field, x, y-1);
+    fill(field, x, y+1);
+    fill(field, x+1, y);
+}
 
 int main() {
-    int n, m;
+    int n,m;
+    vector<vector<int>> field;
     std::cin >> n >> m;
-    int field[n][m];
     for (int i = 0; i < n; ++i) {
+        vector<int> raw;
         for (int j = 0; j < m; ++j) {
             char c;
             std::cin >> c;
-            if (c == '#') field[i][j] = 1;
-            if (c == '.') field[i][j] = 0;
+            if (c == '#') raw.push_back(0);
+            if (c == '.') raw.push_back(1);
         }
+        field.push_back(raw);
     }
     int step = 0;
     for (int x = 0; x < n; ++x) {
         for (int y = 0; y < m; ++y) {
-            if (field[x][y] != 1) continue;
+            if (field[x][y] == 1) continue;
             step++;
-            vector<Point> in_one_link;
-            in_one_link.emplace_back(x, y);
-            while (not in_one_link.empty()) {
-                Point cur = in_one_link.back();
-                in_one_link.pop_back();
-                if (x > 0 and field[x - 1][y] == 1) {
-                    in_one_link.emplace_back(cur.x - 1, cur.y);
-                    field[cur.x - 1][cur.y] = 2;
-                }
-                if (cur.y > 0 and field[cur.x][cur.y - 1] == 1) {
-                    in_one_link.emplace_back(cur.x, cur.y - 1);
-                    field[cur.x][cur.y - 1] = 2;
-                }
-                if (cur.x < n - 1 and field[cur.x + 1][cur.y] == 1) {
-                    in_one_link.emplace_back(cur.x + 1, cur.y);
-                    field[cur.x + 1][cur.y] = 2;
-                }
-                if (cur.y < m - 1 and field[cur.x][cur.y + 1] == 1) {
-                    in_one_link.emplace_back(cur.x, cur.y + 1);
-                    field[cur.x][cur.y + 1] = 2;
-                }
-            }
+            fill(field, x, y);
         }
     }
     std::cout << step;
