3901fd5dc4e1af3f6a1bcecae0cae94b9c5b23f8
oMalyugina <o.v.malyugina@gmail.com>
2019-06-21 15:39:43 +0200

    951

--- graphs/951.cpp
+++ graphs/951.cpp
@@ -15,61 +15,58 @@ struct Point {
     int x;
     int y;
 
-    Point() {};
-
-    Point(int x_, int y_) {
-        x = x_;
-        y = y_;
-    }
+    Point(int x_, int y_): x(x_), y(y_){};
 };
 
 int main() {
     int n, m;
     cin >> n >> m;
-    int field[n][m];
+    bool field[n][m];
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            field[i][j] = 0;
+            field[i][j] = true;
         }
     }
-    int number_healf = n * m;
     int number_points;
     cin >> number_points;
-    number_healf -= number_points;
     vector<Point> points;
     for (int i = 0; i < number_points; i++) {
         int x, y;
         cin >> x >> y;
-        Point point;
-        point.x = x-1;
-        point.y = y-1;
-        points.push_back(point);
-        field[point.x][point.y] = 1;
+        Point point(x - 1, y - 1);
+        if (field[point.x][point.y]) {
+            points.push_back(point);
+            field[point.x][point.y] = false;
+        }
     }
     vector<Point> new_points;
     int step = 0;
-    while (number_healf != 0) {
+    while (!points.empty()) {
         step++;
-        while (points.size() != 0) {
-            Point cur = points[0];
-            points.erase(points.begin());
-            field[cur.x][cur.y] = 2;
-            vector<Point> neigbors;
-            if (cur.x > 0) neigbors.emplace_back(cur.x - 1, cur.y);
-            if (cur.y > 0) neigbors.emplace_back(cur.x, cur.y - 1);
-            if (cur.x < n - 1) neigbors.emplace_back(cur.x + 1, cur.y);
-            if (cur.y < m - 1) neigbors.emplace_back(cur.x, cur.y + 1);
-            for (auto point:neigbors) {
-                if (field[point.x][point.y] == 0) {
-                    field[point.x][point.y] = 1;
-                    new_points.push_back(point);
-                    number_healf--;
-                }
+        while (!points.empty()) {
+            Point cur = points.back();
+            points.pop_back();
+
+            if (cur.x > 0 and field[cur.x - 1][cur.y]) {
+                new_points.emplace_back(cur.x - 1, cur.y);
+                field[cur.x - 1][cur.y] = false;
+            }
+            if (cur.y > 0 and field[cur.x][cur.y - 1]) {
+                new_points.emplace_back(cur.x, cur.y - 1);
+                field[cur.x][cur.y - 1] = false;
+            }
+            if (cur.x < n - 1 and field[cur.x + 1][cur.y]) {
+                new_points.emplace_back(cur.x + 1, cur.y);
+                field[cur.x + 1][cur.y] = false;
+            }
+            if (cur.y < m - 1 and field[cur.x][cur.y + 1]) {
+                new_points.emplace_back(cur.x, cur.y + 1);
+                field[cur.x][cur.y + 1] = false;
             }
         }
         points = new_points;
         new_points.clear();
     }
-    cout << step;
+    cout << --step;
     return 0;
 }
