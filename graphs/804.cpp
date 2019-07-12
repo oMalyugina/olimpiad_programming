cabd62b36c0b98cfdb6d04b3a01c56fd3fd205d0
oMalyugina <o.v.malyugina@gmail.com>
2019-07-12 12:34:25 +0200

    804 wrong answer 8 test

--- graphs/804.cpp
+++ graphs/804.cpp
@@ -2,10 +2,6 @@
 // Created by olga on 11.07.19.
 //
 
-//
-// Created by olga on 05/07/19.
-//
-
 #include <iostream>
 #include <queue>
 
@@ -24,8 +20,8 @@ int main() {
 	int field[m][n];
 	int obstacle = 5000000;
 	Point tiger_pose;
-	Point start = Point(1,1);
-	Point finish = Point(m-2, n-2);
+	Point start = Point(1, 1);
+	Point finish = Point(m - 2, n - 2);
 	for (int j = 0; j < m; ++j) {
 		for (int k = 0; k < n; ++k) {
 			char cell;
@@ -74,28 +70,30 @@ int main() {
 		Point tiger = tiger_step.front();
 		tiger_step.pop();
 
-		if (tiger.x < m - 1 and field[tiger.x + 1][tiger.y] != obstacle and field[tiger.x + 1][tiger.y] + (field[tiger.x][tiger.y] -1) >= 0) {
-			field[tiger.x + 1][tiger.y] = field[tiger.x][tiger.y] -1;
+		if (tiger.x < m - 1 and field[tiger.x + 1][tiger.y] != obstacle and
+		    field[tiger.x + 1][tiger.y] + (field[tiger.x][tiger.y] - 1) >= 0) {
+			field[tiger.x + 1][tiger.y] = field[tiger.x][tiger.y] - 1;
 			tiger_step.push(Point(tiger.x + 1, tiger.y));
 		}
-		if (tiger.x > 0 and field[tiger.x - 1][tiger.y] != obstacle and field[tiger.x - 1][tiger.y]  + (field[tiger.x][tiger.y] -1) >= 0) {
-			field[tiger.x - 1][tiger.y] = field[tiger.x][tiger.y]-1;
+		if (tiger.x > 0 and field[tiger.x - 1][tiger.y] != obstacle and
+		    field[tiger.x - 1][tiger.y] + (field[tiger.x][tiger.y] - 1) >= 0) {
+			field[tiger.x - 1][tiger.y] = field[tiger.x][tiger.y] - 1;
 			tiger_step.push(Point(tiger.x - 1, tiger.y));
 		}
-		if (tiger.y < n - 1 and field[tiger.x][tiger.y+1] != obstacle and field[tiger.x][tiger.y + 1]  + (field[tiger.x][tiger.y] -1) >= 0) {
-			field[tiger.x][tiger.y + 1] = field[tiger.x][tiger.y]-1;
+		if (tiger.y < n - 1 and field[tiger.x][tiger.y + 1] != obstacle and
+		    field[tiger.x][tiger.y + 1] + (field[tiger.x][tiger.y] - 1) >= 0) {
+			field[tiger.x][tiger.y + 1] = field[tiger.x][tiger.y] - 1;
 			tiger_step.push(Point(tiger.x, tiger.y + 1));
 		}
-		if (tiger.y > 0 and field[tiger.x][tiger.y-1] != obstacle and field[tiger.x][tiger.y - 1]  + (field[tiger.x][tiger.y] -1) >= 0) {
-			field[tiger.x][tiger.y - 1] = field[tiger.x][tiger.y]-1;
+		if (tiger.y > 0 and field[tiger.x][tiger.y - 1] != obstacle and
+		    field[tiger.x][tiger.y - 1] + (field[tiger.x][tiger.y] - 1) >= 0) {
+			field[tiger.x][tiger.y - 1] = field[tiger.x][tiger.y] - 1;
 			tiger_step.push(Point(tiger.x, tiger.y - 1));
 		}
 	}
 
-	if (field[finish.x][finish.y] > 0) cout << "YES";
-	else cout << "NO";
-
-
+	if (field[finish.x][finish.y] > 0) cout << "Yes";
+	else cout << "No";
 
 	return 0;
 
